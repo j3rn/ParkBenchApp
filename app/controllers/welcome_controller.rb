bdca164271780b9e6b179fd63a9c5b897ced7d92
Jonathan <j3rn@j3rn.com>
2013-10-08 11:43:02 -0400

    Added Reddit post fetching

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,24 @@
+require 'json'
+require 'httparty'
+
 class WelcomeController < ApplicationController
   def index
+    response = HTTParty.get("http://reddit.com/r/technology/.json")
+    responseHash = JSON.parse response.body
+  
+    numPosts = responseHash["data"]["children"].length
+    for i in 0...numPosts
+      post = Post.new
+           
+      postData = responseHash["data"]["children"][i]["data"]
+      post.author = postData["author"]
+      post.title = postData["title"]
+      post.permalink = postData["permalink"]
+      post.articleUrl = postData["url"]
+
+      post.save
+    end
+    
+    @posts = Post.all
   end
 end
