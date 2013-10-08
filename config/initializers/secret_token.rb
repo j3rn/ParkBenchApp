7020c21e98e86bc06244c64541f584289cc12318
Jonathan <j3rn@j3rn.com>
2013-10-07 21:44:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParkBenchApp::Application.config.secret_key_base = '631fe84b8cdae0693d16781fbb0c286e3b7c0fae692725bfe49cc6837148ac84f204428130822c672e663a55ca5d61a6256608f0ba8461f587c161c5e1235afc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ParkBenchApp::Application.config.session_store :cookie_store, key: '_ParkBenchApp_session'
