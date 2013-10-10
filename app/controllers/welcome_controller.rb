563072abd55bec31c02fcac0980b79bc2c0c99e0
Jonathan <j3rn@j3rn.com>
2013-10-10 09:23:16 -0400

    Modified controller so posts are only fetched if user is logged in for faster page load

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,23 +3,24 @@ require 'httparty'
 
 class WelcomeController < ApplicationController
   def index
-    response = HTTParty.get("http://reddit.com/r/technology/.json")
-    responseHash = JSON.parse response.body
+    if user_signed_in?
+      response = HTTParty.get("http://reddit.com/r/technology/.json")
+      responseHash = JSON.parse response.body
   
-    numPosts = responseHash["data"]["children"].length
-    for i in 0...numPosts
-      post = Post.new
+      numPosts = responseHash["data"]["children"].length
+      for i in 0...numPosts
+        post = Post.new
            
-      postData = responseHash["data"]["children"][i]["data"]
-      post.author = postData["author"]
-      post.title = postData["title"]
-      post.name = postData["name"]
-      post.permalink = postData["permalink"]
-      post.articleUrl = postData["url"]
+        postData = responseHash["data"]["children"][i]["data"]
+        post.author = postData["author"]
+        post.title = postData["title"]
+        post.name = postData["name"]
+        post.permalink = postData["permalink"]
+        post.articleUrl = postData["url"]
 
-      post.save
+        post.save
+      end
+      @posts = Post.paginate(:page => params[:page], :per_page => 10)
     end
-    
-    @posts = Post.paginate(:page => params[:page], :per_page => 10)
   end
 end
